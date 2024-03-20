f6bde76cabf5e48b213bd1feacf4c589235ebc4d
Fabian Bull <spam+github@fbull.de>
2024-03-20 20:29:02 +0100

    Play around with templates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use std::fs;
 
 use clap::{Parser, Subcommand};
+use tera::{Context, Tera};
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let tera = match Tera::new(".zl/templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera
+    };
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -12,8 +29,18 @@ struct Cli {
 #[derive(Subcommand, Debug, Clone)]
 enum Command {
     Validate {
+        /// Path of the markdown file to validate
         #[arg(short, long)]
         path: String,
+    },
+    New {
+        /// Path of the new markdown file
+        #[arg(short, long)]
+        path: String,
+
+        /// Template of the new markdown file
+        #[arg(short, long)]
+        template: String,
     },
 }
 
@@ -24,6 +51,15 @@ fn main() -> Result<(), String> {
         Command::Validate { path } => {
             let input = fs::read_to_string(path).map_err(|_| "File not found")?;
             let _ = markdown::to_mdast(&input, &markdown::ParseOptions::default());
+        },
+        Command::New { path, template } => {
+            let mut context = Context::new();
+            context.insert("title", "test");
+            let expr = TEMPLATES.render(&format!("{}.md", template), &context)
+                .map_err(|e| e.to_string())?;
+
+            fs::write(path, expr)
+                .map_err(|e| format!("Can not write file: {}", e.to_string()))?
         },
     }
 
